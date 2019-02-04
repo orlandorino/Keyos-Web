527df793b05dab604fca132c94cd4ebab7dfb695
John H <johnpaull1997@gmail.com>
2019-02-04 11:31:11 -0500

    Made header more responsive

--- Keyos/ClientApp/app/components/dashboard/dashboard.component.ts
+++ Keyos/ClientApp/app/components/dashboard/dashboard.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import {Chart} from 'chart.js';
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
 const ELEMENT_DATA: PeriodicElement[] = [
   {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
   {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
