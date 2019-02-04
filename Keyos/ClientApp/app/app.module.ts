f31444363557a8eaeae40c1d7eea96e53c1fbf49
John H <johnpaull1997@gmail.com>
2019-02-03 23:46:45 -0500

    Created dashboard component

--- Keyos/ClientApp/app/app.module.ts
+++ Keyos/ClientApp/app/app.module.ts
@@ -3,22 +3,26 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './Util/material.module';
 import { AppComponent } from './app.component';
-import { SignupComponent } from './signup/signup.component';
-import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { LoginComponent } from './components/login/login.component';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { JwtHelper } from 'angular2-jwt';
 import { AppRoutingModule } from './app-routing.module';
-import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import {FlexLayoutModule} from '@angular/flex-layout';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HeaderComponent } from './components/dashboard/subcomponents/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignupComponent,
     LoginComponent,
-    NavbarComponent
+    NavbarComponent,
+    DashboardComponent,
+    HeaderComponent
   
   ],
   imports: [
