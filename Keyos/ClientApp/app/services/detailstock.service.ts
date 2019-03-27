a4e88bab70133c0310bbc110883d09e99fbefed6
nojo <nbjohny@oakland.edu>
2019-03-26 21:26:28 -0400

    merge conflict error resolved

--- Keyos/ClientApp/app/services/detailstock.service.ts
+++ Keyos/ClientApp/app/services/detailstock.service.ts
@@ -57,7 +57,6 @@ symbol:string;
     return this.http.get<ForecastedData[]>("http://localhost:5000/api/buySell/forecastList?stockID="+this.symbol, { headers });
     }
 
-<<<<<<< HEAD
   GetSentimentPercentageData(): Observable<SentimentPercentage[]> {
     let headers = new HttpHeaders().set('Authorization', 'Bearer ' + localStorage.getItem('jwt')).append('Content-Type', 'application/json').append('Access-Control-Allow-Origin', 'https://localhost:5000');
     return this.http.get<SentimentPercentage[]>("http://localhost:5000/api/sentiment/sentimentPercentage?stockID="+this.symbol, { headers });
@@ -65,11 +64,10 @@ symbol:string;
       GetSentimentMessageData(): Observable<SentimentMessage[]> {
     let headers = new HttpHeaders().set('Authorization', 'Bearer ' + localStorage.getItem('jwt')).append('Content-Type', 'application/json').append('Access-Control-Allow-Origin', 'https://localhost:5000');
     return this.http.get<SentimentMessage[]>("http://localhost:5000/api/sentiment/SentimentMessage?stockID="+this.symbol, { headers });
-=======
+      }
     GetActualHistoricalData():Observable<StockTable[]>
     {
       let headers = new HttpHeaders().set('Authorization', 'Bearer ' + localStorage.getItem('jwt')).append('Content-Type', 'application/json').append('Access-Control-Allow-Origin', 'https://localhost:5000');
       return this.http.get<ForecastedData[]>("http://localhost:5000/api/buySell/stocksNotEpoch?stockID="+"aapl", { headers });
->>>>>>> fdedc23f86dd2db03d5ac93eafce6edac16917ba
     }
 }
